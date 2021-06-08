3a3b9fc5922fd2ac8b3255ffaa67742bd8bea1a9
Ruchir Bisht <ruchircr7@gmail.com>
2021-06-08 19:21:25 +0530

    modified the newsAPI

--- Web/server.js
+++ Web/server.js
@@ -23,7 +23,7 @@ app.use("/", require("./routes/guest"));
 
 // News Api
 const newsURL =
-  "https://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=" +
+  "https://newsapi.org/v2/everything?q=computer+student+education&apiKey=" +
   process.env.NEWS_API_KEY;
 
 //home page
@@ -34,15 +34,16 @@ app.get("/", async (req, res) => {
   try {
     const response = await fetch(newsURL);
     const data = await response.json();
+
     let news = data.articles;
-    //console.log(data.articles.length);
+    console.log(data.articles.length);
     //console.log(news.length);
     if (!news) {
       news = [];
     } else {
       news = news.filter(article => article.urlToImage !== null);
-      //console.log("Total news Result: " + news.length);
-      //res.send(news);
+      console.log("Total news Result: " + news.length);
+
       if (news.length > 30) {
         news = news.splice(0, 30);
       }
